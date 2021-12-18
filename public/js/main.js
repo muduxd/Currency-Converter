b1eee2978df9bb2c943b2ee9e908e2b0392063ce
Mudura Razvan <razvanmudura25@gmail.com>
2021-12-18 23:25:21 +0200

    commit

---
+++ public/js/main.js
@@ -0,0 +1,63 @@
+const baseCurrency = document.querySelector("#base-currency");
+const toCurrency = document.querySelector("#to-currency");
+
+const baseInput = document.querySelector("#base-input");
+const toInput = document.querySelector("#to-input");
+
+let currencies = [];
+let rate = 1;
+
+const fetchCurrencies = async () => {
+  try {
+    const result = await fetch("http://localhost:8080/getCurrencies");
+    return await result.json();
+  } catch (e) {
+    return;
+  }
+};
+
+const changeCurrency = async () => {
+  if (baseCurrency.value && toCurrency.value)
+    try {
+      const result = await fetch(`http://localhost:8080/getRate?base_currency=${baseCurrency.value}`);
+      return await result.json();
+    } catch (e) {
+      return;
+    }
+  return;
+};
+
+fetchCurrencies().then((result) => {
+  currencies = result;
+
+  currencies.forEach((currency) => {
+    const option1 = document.createElement("option");
+    const option2 = document.createElement("option");
+
+    option1.value = currency;
+    option2.value = currency;
+
+    option1.textContent = currency;
+    option2.textContent = currency;
+
+    baseCurrency.appendChild(option1);
+    toCurrency.appendChild(option2);
+  });
+});
+
+baseCurrency.addEventListener("change", () =>
+  changeCurrency().then((result) => {
+    rate = result[toCurrency.value];
+    toInput.value = (baseInput.value * rate).toFixed(2);
+  })
+);
+
+toCurrency.addEventListener("change", () =>
+  changeCurrency().then((result) => {
+    rate = result[toCurrency.value];
+    baseInput.value = (toInput.value / rate).toFixed(2);
+  })
+);
+
+baseInput.addEventListener("keyup", () => (toInput.value = (baseInput.value * rate).toFixed(2)));
+toInput.addEventListener("keyup", () => (baseInput.value = (toInput.value / rate).toFixed(2)));
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const req = require("request");
+const path = require("path");
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+const BASE_URL = process.env.URL;
+
+//!-MIDDLEWARE-
+
+app.set("view engine", "hbs");
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
+//!-ROUTES-
+
+app.get("/", (request, response) => {
+  response.render("main");
+});
+
+app.get("/getRate", (request, response) => {
+  const baseCurrency = request.query.base_currency;
+  const url = `${BASE_URL}&base_currency=${baseCurrency}`;
+
+  req({ url, json: true }, (error, { body }) => {
+    if (error) response.status(500).send({ error: "Invalid Request" });
+    else response.status(201).send(body.data);
+  });
+});
+
+app.get("/getCurrencies", (request, response) => {
+  req({ url: BASE_URL, json: true }, (error, { body }) => {
+    if (error) response.status(500).send({ error: "Invalid Request" });
+    else response.status(201).send(Object.keys(body.data));
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is active at http://localhost:${PORT}`));

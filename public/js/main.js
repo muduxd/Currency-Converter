8646a2813ec04e72ec611825ed3d642c72c3d52a
Mudura Razvan <razvanmudura25@gmail.com>
2022-09-17 01:12:22 +0300

    synthax

--- public/js/main.js
+++ public/js/main.js
@@ -1,75 +1,82 @@
-const baseCurrency = document.querySelector("#base-currency");
-const toCurrency = document.querySelector("#to-currency");
+const baseCurrency = document.querySelector("#base-currency")
+const toCurrency = document.querySelector("#to-currency")
 
-const baseInput = document.querySelector("#base-input");
-const toInput = document.querySelector("#to-input");
+const baseInput = document.querySelector("#base-input")
+const toInput = document.querySelector("#to-input")
 
-let currencies = [];
-let rate = 1;
+let currencies = []
+let rate = 1
+
 
 const fetchCurrencies = async () => {
   try {
-    const result = await fetch("/getCurrencies");
-    return await result.json();
-  } catch (e) {
-    return;
+    const result = await fetch("/getCurrencies")
+    return await result.json()
+  } 
+  catch (e) {
+    return
   }
-};
+}
+
 
 const changeCurrency = async () => {
   if (baseCurrency.value && toCurrency.value)
     try {
-      const result = await fetch(`/getRate?base_currency=${baseCurrency.value}`);
-      return await result.json();
-    } catch (e) {
-      return;
+      const result = await fetch(`/getRate?base_currency=${baseCurrency.value}`)
+      return await result.json()
+    } 
+    catch (e) {
+      return
     }
-  return;
-};
+
+  return
+}
+
 
 fetchCurrencies().then(({ currencies, symbols }) => {
   currencies.forEach((currency, index) => {
     if (currency !== "BTC") {
-      const option1 = document.createElement("option");
-      const option2 = document.createElement("option");
+      const option1 = document.createElement("option")
+      const option2 = document.createElement("option")
 
-      option1.value = currency;
-      option2.value = currency;
+      option1.value = currency
+      option2.value = currency
 
-      option1.textContent = currency;
-      option2.textContent = currency;
+      option1.textContent = currency
+      option2.textContent = currency
 
       if (symbols[index]) {
-        option1.textContent += ` - ${symbols[index]}`;
-        option2.textContent += ` - ${symbols[index]}`;
+        option1.textContent += ` - ${symbols[index]}`
+        option2.textContent += ` - ${symbols[index]}`
       }
 
       if (currency === "EUR") {
-        option1.selected = true;
-        option2.selected = true;
+        option1.selected = true
+        option2.selected = true
       }
 
-      baseCurrency.appendChild(option1);
-      toCurrency.appendChild(option2);
+      baseCurrency.appendChild(option1)
+      toCurrency.appendChild(option2)
     }
-  });
-});
+  })
+})
 
 baseCurrency.addEventListener("change", () =>
   changeCurrency().then((result) => {
-    rate = result[toCurrency.value];
+    rate = result[toCurrency.value]
 
-    toInput.value = baseInput.value * rate;
+    toInput.value = baseInput.value * rate
   })
-);
+)
 
 toCurrency.addEventListener("change", () =>
   changeCurrency().then((result) => {
-    rate = result[toCurrency.value];
+    rate = result[toCurrency.value]
 
-    baseInput.value = toInput.value / rate;
+    baseInput.value = toInput.value / rate
   })
-);
+)
 
-baseInput.addEventListener("keyup", () => (toInput.value = baseInput.value * rate));
-toInput.addEventListener("keyup", () => (baseInput.value = toInput.value / rate));
+
+baseInput.addEventListener("keyup", () => (toInput.value = baseInput.value * rate))
+toInput.addEventListener("keyup", () => (baseInput.value = toInput.value / rate))
--- src/server.js
+++ src/server.js
@@ -1,50 +1,62 @@
-const getSymbolFromCurrency = require("currency-symbol-map");
-const express = require("express");
-const req = require("request");
-const path = require("path");
+const getSymbolFromCurrency = require("currency-symbol-map")
+const express = require("express")
+const axios = require("axios")
+const path = require("path")
 
-require("dotenv").config();
-const app = express();
-const PORT = process.env.PORT || 8080;
-const BASE_URL = process.env.URL;
+require("dotenv").config()
+const app = express()
+const PORT = process.env.PORT || 8080
+const BASE_URL = process.env.URL
+
 
 //!-MIDDLEWARE-
 
-app.set("view engine", "hbs");
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.set("view engine", "hbs")
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "../public")))
+
 
 //!-ROUTES-
 
-app.get("/", (request, response) => response.render("main"));
+app.get("/", (request, response) => response.render("main"))
 
-app.get("/getRate", (request, response) => {
-  const baseCurrency = request.query.base_currency;
-  const url = `${BASE_URL}&base_currency=${baseCurrency}`;
+app.get("/getRate", async (request, response) => {
+  const baseCurrency = request.query.base_currency
+  const url = `${BASE_URL}&base_currency=${baseCurrency}`
 
-  req({ url, json: true }, (error, { body }) => {
-    if (error) response.status(500).send({});
-    else response.status(201).send(body.data);
-  });
-});
+  try {
+    const {data} = await axios.get(url)
 
-app.get("/getCurrencies", (request, response) => {
-  req({ url: BASE_URL, json: true }, (error, { body }) => {
-    if (error) response.status(500).send({});
-    else {
-      const currencyList = Object.keys(body.data);
-      currencyList.push("USD");
+    return response.status(201).send(data.data)
+  }
+  catch(e) {
+    console.log(e)
+    return response.status(500).send({})
+  }
 
-      response.status(201).send({
-        currencies: currencyList.sort(),
-        symbols: currencyList.sort().map((currency) => getSymbolFromCurrency(currency)),
-      });
-    }
-  });
-});
+})
 
-app.get("*", (request, response) => response.render("404"));
+app.get("/getCurrencies", async (request, response) => {
+  try {
+    const {data} = await axios.get(BASE_URL)
+    const currencyList = Object.keys(data.data)
+
+    currencyList.push("USD")
+
+    return response.status(201).send({
+      currencies: currencyList.sort(),
+      symbols: currencyList.sort().map((currency) => getSymbolFromCurrency(currency)),
+    })
+  }
+  catch(e) {
+    return response.status(500).send({})
+  }
+
+})
+
+app.get("*", (request, response) => response.render("404"))
+
 
 //!-LISTEN-
 
-app.listen(PORT);
+app.listen(PORT)
